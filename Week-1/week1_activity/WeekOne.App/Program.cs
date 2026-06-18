5419c0c028ef0443ad634b54058ec6076abaf313
Stefano <stefano.herrejon@hotmail.com>
2026-06-17 20:08:56 -0600

    Correccion codigo base

--- Week-1/week1_activity/WeekOne.App/Program.cs
+++ Week-1/week1_activity/WeekOne.App/Program.cs
@@ -8,27 +8,26 @@ public class Program
     public static void Main()
     {
 
-        var running = true;
-        while (running)
-        {
-            //welcome message to menu- pending to add
-            // -- add sumary of final rental - total bill, days, car
-            // -- not to rent unavailable cars
-            // -- make sure car is available - otheriwse throw exception
-            //  not to rent below the minimum rental period
-            
+        bool running = true;
+        List<(CarRental coche, int dias)> rentados = new(); //Lista de coches rentador por el usuario. Dupla para tener coche y dias
+        
 
+        Console.WriteLine("Welcome!");
+        while (running)
+        {            
             Commands.PrintMenu();
             int choice = int.Parse(Console.ReadLine());   // naive: may throw on bad input — fine for now
             switch (choice)
             {
                 case 1: Commands.PrintInventory(); break;
                 case 2: Commands.AddCar(); break;
-                case 3: Commands.RentCar(); break;
+                case 3: Commands.RentCar(rentados); break;
                 case 0: running = false; break;
             }
         }
 
+        Commands.RentedCarsInfo(rentados);
+        Console.WriteLine("Good Bye!");
     }
     
 }
