1c24ccbb888ded03a6bdff31f3afb03b125a299e
Axel mac <axelhernandezperez23@gmail.com>
2026-07-14 08:42:36 -0600

    /reports/rank-of/{units:int} - Adding report, return rank based on given units sold

--- Week-5-solo-project/AutoSupply.Api/Program.cs
+++ Week-5-solo-project/AutoSupply.Api/Program.cs
@@ -268,6 +268,44 @@ app.MapGet("/reports/top-products", async (AutoSupplyDbContext db, CancellationT
 });
 
 
+
+app.MapGet("/reports/rank-of/{units:int}", async (int units, AutoSupplyDbContext db, CancellationToken ct) => {
+    var unitsDesc = await db.FulfillmentEvents
+        .Where(e => e.Type == FulfillmentEventType.Fulfilled)
+        .Join(
+            db.OrderLines,
+            e => e.OrderId,
+            line => line.OrderId,
+            (e, line) => line)
+        .GroupBy(line => line.ProductId)
+        .Select(g => g.Sum(line => line.Quantity))
+        .OrderByDescending(row => row)
+        .ToArrayAsync(ct);
+
+        //Binary search
+        var index = Array.BinarySearch(
+            unitsDesc,
+            units,
+            Comparer<int>.Create((a, b) => b.CompareTo(a))
+        );
+
+
+
+        return Results.Ok(new
+        {
+            units,
+            rank = index >= 0 ? index + 1 : -1
+        });
+});
+
+
+
+
+
+
+
+
+
 app.MapPost("/benchmark", async (
     BenchmarkRequest request,
     ISeeder seeder,
